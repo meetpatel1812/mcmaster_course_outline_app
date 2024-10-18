a083f47646cfc3c75b4d8df14442f7c52ec0bf52
meetpatel1812 <71310616+meetpatel1812@users.noreply.github.com>
2024-10-18 00:22:56 -0400

    Update pdf_data.py

--- pdf_data.py
+++ pdf_data.py
@@ -191,25 +191,4 @@ pdfs = [
 
     # Add more PDFs as needed
 
-    {
-    "name": "meet",
-    "label": "meet",
-    "category": "Professional Development course",
-    "subcategory": "Automotive Stream",
-    "semesters": [
-        "Fall"
-    ],
-    "file_path": "Course/Professional Development course/Meet_Transcript.pdf",
-    "icon": ""
-},
-    {
-    "name": "meet",
-    "label": "meet2",
-    "category": "Other elective course",
-    "subcategory": "Automation and Smart Systems",
-    "semesters": [
-        "Fall"
-    ],
-    "file_path": "Course/Meet_Transcript.pdf",
-    "icon": ""
-},]
+   ]
